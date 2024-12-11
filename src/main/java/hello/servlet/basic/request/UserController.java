1536262b5cef3deb499f01f7a63ef1ab2faa397c
baesm971013 <bobbyb537@naver.com>
2024-12-11 15:50:04 +0900

    feat: http  api json

--- src/main/java/hello/servlet/basic/request/UserController.java
+++ src/main/java/hello/servlet/basic/request/UserController.java
@@ -1,5 +1,6 @@
 package hello.servlet.basic.request;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.ServletInputStream;
 import jakarta.servlet.annotation.WebServlet;
@@ -11,14 +12,21 @@ import org.springframework.util.StreamUtils;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 
-@WebServlet(name = "requestBodyStringServlet", urlPatterns = "/request-body-string")
-public class RequestBodyStringServlet extends HttpServlet {
+@WebServlet(name = "userController", urlPatterns = "/user")
+public class UserController extends HttpServlet {
+    private ObjectMapper objectMapper = new ObjectMapper();
     @Override
     protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         ServletInputStream inputStream =request.getInputStream();
         String messageBody = StreamUtils.copyToString(inputStream, StandardCharsets.UTF_8);
-        System.out.println("messageBody = " + messageBody);
+
+        UserData user = objectMapper.readValue(messageBody, UserData.class);
+        System.out.println("username" + user.getUsername());
+        System.out.println("age" + user.getAge());
+        System.out.println("email" + user.getEmail());
+        System.out.println("department" + user.getDepartment());
 
         response.getWriter().write("ok");
+
     }
 }
---
+++ src/main/java/hello/servlet/basic/request/UserData.java
@@ -0,0 +1,14 @@
+package hello.servlet.basic.request;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class UserData {
+    private String username;
+    private int age;
+    private String department;
+
+    private String email;
+
+}
